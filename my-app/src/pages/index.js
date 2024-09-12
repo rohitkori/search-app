32f057188f47b625d5e58c05a82b5441cfe2971e
Rohit <kori.1@iitj.ac.in>
2024-09-13 02:51:51 +0530

    Update homepage UI
    Signed-off-by: Rohit <kori.1@iitj.ac.in>

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,13 +1,9 @@
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-} from "@/components/ui/navigation-menu";
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
 import Navbar from "@/components/navbar";
+
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 // Import the CSS module
 import styles from "@/styles/home.module.css";
@@ -15,26 +11,35 @@ import styles from "@/styles/home.module.css";
 export default function Home() {
   return (
     <>
-      {/* <header className={styles.navbar}>
-          <NavigationMenu>
-              <NavigationMenuList className={styles.navMenuList}>
-                  <NavigationMenuItem className={styles.navItem}>
-                      <a href="/hello">hello</a>
-                  </NavigationMenuItem>
-                  <NavigationMenuItem className={styles.navItem}>
-                      <NavigationMenuTrigger className={styles.navTrigger}>Item Two</NavigationMenuTrigger>
-                      <NavigationMenuContent className={styles.navContent}>
-                          <NavigationMenuLink className={styles.navLink}>Link 3</NavigationMenuLink>
-                          <NavigationMenuLink className={styles.navLink}>Link 4</NavigationMenuLink>
-                      </NavigationMenuContent>
-                  </NavigationMenuItem>
-              </NavigationMenuList>
-          </NavigationMenu>
-      </header> */}
       <Navbar />
       <div className={styles.container}>
-    <Input type="email" placeholder="Email" />
-    </div>
-      </>
+        <div className={styles.content}>
+          <main className={styles.mainSection}>
+            <div className={styles.logoSection}>
+              <Image
+                src="/Group 1.svg"
+                alt="Girman Logo"
+                width={100}
+                height={50}
+              />
+              <Image
+                src="/Girman.svg"
+                alt="Girman SVG Logo"
+                width={200}
+                height={100}
+              />
+            </div>
+            <div className={styles.searchBarContainer}>
+              <Search className={styles.searchIcon} />
+              <input
+                className={styles.searchInput}
+                type="text"
+                placeholder="Search..."
+              />
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
   );
 }
